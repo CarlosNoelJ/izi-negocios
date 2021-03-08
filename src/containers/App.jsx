241cc9604042a3987cac890a56a5b46b744c4a90
CarlosNoelJ <carlos.noel@grupobusiness.it>
2021-03-07 21:56:35 -0500

    update

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../assets/Styles/App.scss'
-import About from '../components/About'
-import Header from '../components/Header'
+// import About from '../components/About'
+// import Header from '../components/Header'
 import Products from '../components/Products'
 import Hero from '../components/Hero'
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = () => {
